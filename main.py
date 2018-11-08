c55d67fbaabf5fad30b6bf9987f587e57a47802b
Travis Tang <ga86yam@mytum.de>
2018-11-08 09:21:35 +0000

    camera with brightness adjustment

---
+++ main.py
@@ -0,0 +1,23 @@
+from webthing import (Action, Event, Property, Thing, WebThingServer,SingleThing,Value)
+
+import logging
+import time
+from picamera import PiCamera
+from functools import partial
+from flask import Flask, send_file, jsonify, request
+
+app = Flask(__name__)
+# set up the camera
+camera = PiCamera()
+
+@app.route('/properties/brightness',methods = ['GET','PUT'])
+def brightness():
+    if request.method == 'GET':
+        payload = {'brightness': camera.brightness}
+        return jsonify(payload)
+    else:
+        data = request.get_json()
+        if 'brightness' in data and data['brightness'] in range(0,100 + 1):
+            camera.brightness = data['brightness']
+        return jsonify(data)
+
