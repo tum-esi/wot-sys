d61471f45b968c46ec2c00c4bb79424852f8f8d2
Travis Tang <ga86yam@mytum.de>
2018-11-12 11:11:34 +0000

    Change configuration as property; Add size of frame as property

--- main.py
+++ main.py
@@ -9,21 +9,25 @@ from flask import Flask, send_file, jsonify, request
 app = Flask(__name__)
 # set up the camera
 camera = PiCamera()
-
-@app.route('/properties/brightness',methods = ['GET','PUT'])
-def brightness():
+size = (640,480)
+@app.route('/properties/configuration',methods = ['GET','PUT'])
+def configuration():
     if request.method == 'GET':
-        payload = {'brightness': camera.brightness}
+        payload = {'brightness': camera.brightness, 'size': size}
         return jsonify(payload)
     else:
+        global size
         data = request.get_json()
         if 'brightness' in data and data['brightness'] in range(0,100 + 1):
             camera.brightness = data['brightness']
+        if 'size' in data and len(data['size']) == 2 and not any(map(lambda s: s < 0, data['size'])):
+            size = tuple(data['size'])
         return jsonify(data)
 
 @app.route('/properties/frame',methods = ['GET'])
 def shot():
-    camera.capture('frame.jpg')
+    print("size, {}".format(size))
+    camera.capture('frame.jpg',resize = size)
     return send_file('frame.jpg')
 
 if __name__ == '__main__':
