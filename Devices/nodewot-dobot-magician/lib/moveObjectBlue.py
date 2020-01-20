39a5f0b6d17e114ab54aec112e746a03c8f647f2
ga45kab <ga45kab@mytum.de>
2020-01-20 12:53:24 +0100

    Upload New File

---
+++ Devices/nodewot-dobot-magician/lib/moveObjectBlue.py
@@ -0,0 +1,14 @@
+from serial.tools import list_ports
+
+from pydobot import Dobot
+
+port = list_ports.comports()[0].device
+device = Dobot(port=port, verbose=True)
+
+(x, y, z, r, j1, j2, j3, j4) = device.pose()
+(l) = device.pose_l()
+
+device.move_to_with_l(228, 23, -40, 5, 500, wait=True) # we wait until this movement is done before continuing
+device.grip(False)
+device.suck(False)
+device.close()
