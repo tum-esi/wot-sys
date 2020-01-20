c2e48e09c416ffbdb2003bcb38f3f5bf52c8130f
ga45kab <ga45kab@mytum.de>
2020-01-20 12:57:19 +0100

    Upload New File

---
+++ Devices/nodewot-dobot-magician/lib/archieve/actionA.py
@@ -0,0 +1,20 @@
+from serial.tools import list_ports
+
+from pydobot import Dobot
+
+port = list_ports.comports()[0].device
+device = Dobot(port=port, verbose=True)
+
+(x, y, z, r, j1, j2, j3, j4) = device.pose()
+(l) = device.pose_l()
+
+device.grip(False)
+device.move_to_with_l(228, -65, 20, -17, 0, wait=True) # we wait until this movement is done before continuing
+device.move_to_with_l(228, -65, -47, -17, 0, wait=True) # we wait until this movement is done before continuing
+device.grip(True)
+device.move_to_with_l(228, -65, -47, -17, 0, wait=True) # we wait until this movement is done before continuing
+device.move_to_with_l(228, -65, 20, -17, 0, wait=True) # we wait until this movement is done before continuing
+device.move_to_with_l(228, -65, 40, -17, 211, wait=True) # we wait until this movement is done before continuing
+device.grip(False)
+device.suck(False)
+device.close()
