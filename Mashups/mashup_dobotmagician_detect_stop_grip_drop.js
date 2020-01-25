b8244926eb3449fdd8869ec94798196f32ffc0fe
ga45kab <ga45kab@mytum.de>
2020-01-25 19:50:42 +0100

    Upload New File

---
+++ Mashups/mashup_dobotmagician_detect_stop_grip_drop.js
@@ -0,0 +1,99 @@
+/*
+This is a mashup that uses the devices of the dobot magician setup and is based on EVENTS for triggering ACTIONS.
+
+Description:
+
+This mashup contains the following things:
+    - two infrared sensors
+    - two conveyor belts, moved by stepper motors
+    - one dobot magician
+
+Each conveyor belt starts at the beginning, and stops when the infrared sensor detects an object. After that, the robot the robot moves the object still to the same specified position. 
+After that the robot is ready for a new command from the same conveyor belt or for a command from the other conveyor belt. At the end of the moving operation, the conveyor belt 
+restarts automatically again. 
+
+*/
+
+const Robot_TD_ADDRESS = "http://192.168.0.127:8080/DobotMagician";
+const Infrared1_TD_ADDRESS = "http://192.168.0.128:8080/InfraredSensor1";
+const Infrared2_TD_ADDRESS = "http://192.168.0.129:8080/InfraredSensor2";
+const ConveyorBelt1_TD_ADDRESS = "http://192.168.0.130:8080/StepperMotor";
+const ConveyorBelt2_TD_ADDRESS = "http://192.168.0.131:8080/StepperMotor";
+
+WoTHelpers.fetch(Robot_TD_ADDRESS).then(async (robotTD) => {
+    let robotThing = await WoT.consume(robotTD);
+    console.info("=== TD ===");
+    console.info(robotTD);
+    console.info("==========");
+
+    // Move dobot magician to default start position
+    robotThing.invokeAction("startPosition");
+
+    WoTHelpers.fetch(ConveyorBelt1_TD_ADDRESS).then(async (conveyorbelt1TD) => {
+        let conveyorThing1 = await WoT.consume(conveyorbelt1TD);
+
+            WoTHelpers.fetch(Infrared1_TD_ADDRESS).then(async (infrared1TD) => {
+                let infraredThing1 = await WoT.consume(infrared1TD);
+
+                // Start moving conveyor belt 1
+                conveyorThing1.invokeAction("startBeltForward");
+                // In case of an event of the infrared sensor, call function detectedObjectPosition1
+                infraredThing1.subscribeEvent("detectedObject",
+                x => detectedObjectPosition1(),
+                e => console.error("Error: %s", e),
+                () => console.info("Completed")
+                );
+
+                async function detectedObjectPosition1(){
+                    // Stop the conveyor belt
+                    await conveyorThing1.invokeAction("stopBelt");
+                    // The robot picks the object
+                    await robotThing.invokeAction("pickObjectPosition1");
+                    // The robot moves the object to a specified position
+                    await robotThing.invokeAction("moveObjectNone");
+                    // Restart the conveyor belt
+                    await conveyorThing1.invokeAction("startBeltForward");
+                }
+    
+            }).catch( ()=>{
+                console.log("Could not get the ConveyorBelt1 TD")
+            });
+    }).catch( ()=>{
+        console.log("Could not get the Infrared1 TD")
+    });   
+
+    WoTHelpers.fetch(ConveyorBelt2_TD_ADDRESS).then(async (conveyorbelt2TD) => {
+        let conveyorThing2 = await WoT.consume(conveyorbelt2TD);
+
+            WoTHelpers.fetch(Infrared2_TD_ADDRESS).then(async (infrared2TD) => {
+                let infraredThing2 = await WoT.consume(infrared2TD);
+
+               // Start moving conveyor belt 2
+               conveyorThing2.invokeAction("startBeltForward");
+               // In case of an event of the infrared sensor, call function detectedObjectPosition2
+               infraredThing2.subscribeEvent("detectedObject",
+               x => detectedObjectPosition2(),
+               e => console.error("Error: %s", e),
+               () => console.info("Completed")
+               );
+
+               async function detectedObjectPosition2(){
+                    // Stop the conveyor belt
+                    await conveyorThing2.invokeAction("stopBelt");
+                    // The robot picks the object
+                    await robotThing.invokeAction("pickObjectPosition2");
+                    // The robot moves the object to a specified position
+                    await robotThing.invokeAction("moveObjectNone");
+                    // Restart the conveyor belt
+                    await conveyorThing2.invokeAction("startBeltForward");
+                }
+
+            }).catch( ()=>{
+                console.log("Could not get the ConveyorBelt2 TD")
+            });
+    }).catch( ()=>{
+        console.log("Could not get the Infrared2 TD")
+    });         
+}).catch( ()=>{
+    console.log("Could not get the Robot TD")
+});
