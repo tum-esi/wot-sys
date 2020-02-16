923abbb18fb2dba8e9a497f21adc0c051fdc49e0
ga45kab <ga45kab@mytum.de>
2020-02-16 23:27:04 +0100

    Replace moveObjectRed.py

--- Devices/nodewot-dobot-magician/lib/moveObjectRed.py
+++ Devices/nodewot-dobot-magician/lib/moveObjectRed.py
@@ -8,6 +8,7 @@ device = Dobot(port=port, verbose=True)
 (x, y, z, r, j1, j2, j3, j4) = device.pose()
 (l) = device.pose_l()
 
+device.move_to_with_l(x, y, 20, r, l, wait=True) # we wait until this movement is done before continuing
 device.move_to_with_l(228, 23, -40, 5, 200, wait=True) # we wait until this movement is done before continuing
 device.grip(False)
 device.suck(False)
